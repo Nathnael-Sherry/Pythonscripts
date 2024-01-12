b86ae64b2155e5d8cf18c39a59094d70d25e9092
unknown <nathnaelsherry@outlook.com>
2024-01-12 10:58:39 +0300

    Functions

--- Functions.py
+++ Functions.py
@@ -6,13 +6,21 @@
 #     print(text)
 # print_kevin()
 
-# If statement with a function
-def school_age_calculator(age,name):
-    if age < 5:
-        print("Enjoy the time!", name, "is only",age)
-    elif age == 5:
-        print("Enjoy Kindergarten", name)
-    else:
-        print("They grow up so fast!")
+# # If statement with a function
+# def school_age_calculator(age,name):
+#     if age < 5:
+#         print("Enjoy the time!", name, "is only",age)
+#     elif age == 5:
+#         print("Enjoy Kindergarten", name)
+#     else:
+#         print("They grow up so fast!")
 
-school_age_calculator(5,"Nathan")
+# school_age_calculator(5,"Nathan")
+
+# Getting a parameter from a function
+def add_ten_to_age(age):
+    new_age = age + 10
+    return new_age
+
+How_old_will_I_be = add_ten_to_age(3)
+print(How_old_will_I_be)
